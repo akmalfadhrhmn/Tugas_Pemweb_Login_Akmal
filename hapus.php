cc2bcc9e60811c2a9960dea2c4caa446237d5186
akmal <akmalfadhurohman@gmail.com>
2025-05-22 18:31:54 +0700

    Maaf kak waktunya commit nya keubah , soalnya tadi saya waktu kuis salah remote jadi push nya malah ke sini bukan ke repo kuis

--- hapus.php
+++
@@ -1,6 +0,0 @@
-<?php
-include "db.php";
-$id = $_GET['id'];
-mysqli_query($conn, "DELETE FROM users WHERE id=$id");
-header("Location: crud.php");
-?>
---
+++ password_hash.php
@@ -0,0 +1,9 @@
+<?php
+
+$password_hash = password_hash(password:"12345", algo:PASSWORD_DEFAULT);
+
+
+
+echo $password_hash;
+
+?>
